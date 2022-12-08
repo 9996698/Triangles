7587cc85572d2d450670774059aea97b8ee8e4a0
9996698 <ise.gura@bsu.by>
2022-12-09 01:08:46 +0300

    Create new methods to determine types of Triangle

--- main.py
+++ main.py
@@ -46,37 +46,58 @@ class Triangle(Shape):
         self.__b = b
         self.__c = c
         self.__type = self.__qualify_type(self.__get_side(a, b), self.__get_side(b, c), self.__get_side(c, a))
-        self.__type_angle = self.__qualify_type_angle(self.__get_side(a, b), self.__get_side(b, c), self.__get_side(c, a))
+        self.__type_angle = self.__qualify_type_angle(self.__get_side(a, b), self.__get_side(b, c),
+                                                      self.__get_side(c, a))
         super().__init__()
 
     @staticmethod
     def __get_side(p1: Point, p2: Point) -> float:
         return ((p2.get_x() - p1.get_x()) ** 2 + (p2.get_y() - p1.get_y()) ** 2) ** 0.5
 
-    def __qualify_type(self, ab: float, bc: float, ca: float) -> float:
-        pass
+    @staticmethod
+    def __qualify_type(ab: float, bc: float, ca: float):
+        if ab == bc == ca:
+            return "equilateral"
+        elif (ab == ca != bc) or (ab == bc != ca) or (bc == ca != ab):
+            return "isosceles"
+        elif ab != bc != ca:
+            return "arbitrary"
 
-    def __qualify_type_angle(self, ab: float, bc: float, ca: float) -> float:
-        pass
+    @staticmethod
+    def __qualify_type_angle(ab: float, bc: float, ca: float):
+        if round(ab ** 2, 9) == bc ** 2 + ca ** 2 or \
+                round(bc ** 2, 9) == ab ** 2 + ca ** 2 or \
+                round(ca ** 2, 9) == bc ** 2 + ab ** 2:
+            return "right - angled"
+
+        elif round(ab ** 2, 9) < bc ** 2 + ca ** 2 and \
+                round(bc ** 2, 9) < ab ** 2 + ca ** 2 and \
+                round(ca ** 2, 9) < bc ** 2 + ab ** 2:
+            return "acute - angled"
+
+        else:
+            return "obtuse - angled"
 
     def _calculate_perimeter(self):
-        return self.__a + self.__b + self.__c
+        return self.__get_side(self.__a, self.__b) + self.__get_side(self.__b, self.__c) \
+               + self.__get_side(self.__c, self.__a)
 
     def _calculate_area(self):
         p = self.__a + self.__b + self.__c
-        s = p/2
+        s = p / 2
         s1 = 1
         args = [self.__a, self.__b, self.__c]
         for arg in args:
             s1 *= (s - arg)
-        return (s * s1)**0.5
+        return (s * s1) ** 0.5
 
     def __str__(self):
-        pass
+        return self.__type, self.__type_angle
 
 
 if __name__ == '__main__':
-    first_point = Point(1, 3)
-    second_point = Point(5, 4)
-    third_point = Point(7, 2)
-
+    first_point = Point(0, 5)
+    second_point = Point(0, 0)
+    third_point = Point(5, 0)
+    Triangle1 = Triangle(first_point, second_point, third_point)
+    print(Triangle1.__str__())
